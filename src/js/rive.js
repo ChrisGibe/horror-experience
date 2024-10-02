0c04efe3993ca207e21b265f4a1be48f00d2f58e
Christophe Gibelli <cgibelli@tequilarapido.com>
2024-10-02 21:23:20 +0200

    Init Rive app with our first canvas

---
+++ src/js/rive.js
@@ -0,0 +1,25 @@
+import * as rive from "@rive-app/canvas";
+
+export const initRive = () => {
+
+    // Fit to the width and align to the top of the canvas
+    let layout = new rive.Layout({
+        fit: rive.Fit.FitWidth,
+        alignment: rive.Alignment.TopCenter,
+    });
+
+    const r = new rive.Rive({
+        src: "./src/assets/halloween.riv",
+        // OR the path to a discoverable and public Rive asset
+        // src: '/public/example.riv',
+        canvas: document.getElementById("canvas"),
+        autoplay: true,
+        // artboard: "Arboard", // Optional. If not supplied the default is selected
+        onLoad: () => {
+          r.resizeDrawingSurfaceToCanvas();
+        },
+    });
+
+    // Update the layout
+    r.layout = new rive.Layout({ fit: rive.Fit.Fill });
+}
